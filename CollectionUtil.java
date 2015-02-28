12f6a7a31e1bd0f75d7065c5c5b2fd45ad9801a3
Dolly <dollyg@INDOLLYG.corporate.thoughtworks.com>
2015-02-28 14:30:50 +0530

    REDUCE Done.

--- CollectionUtil.java
+++ CollectionUtil.java
@@ -1,18 +1,30 @@
 import java.util.*;
 
+interface ListMapper<E,K> {
+	K callback(E ele, int index, List<E> list);
+}
+
+interface ListFilter<E> {
+	boolean predicate(E ele, int index, List<E> list);
+}
+
+interface ListReducer<E,K> {
+	K reducer(K pv, E cv, int index, List<E> list);
+}
+
 public class CollectionUtil {
-	public static<E> List<E> map(List<E> list, ListMapper listMapper) {
-		List<E> result = new ArrayList();
+	public static<E,K> List<K> map(List<E> list, ListMapper<E,K> listMapper) {
+		List<K> result = new ArrayList<K>();
 		int index = -1;
 		for(E ele : list) {
-			E i = (E)listMapper.callback(ele,++index,list);
+			K i = listMapper.callback(ele,++index,list);
 			result.add(i);
 		}
 		return result;
 	}
 
-	public static<E> List<E> filter(List<E> list, ListFilter listFilter) {
-		List<E> result = new ArrayList();
+	public static<E> List<E> filter(List<E> list, ListFilter<E> listFilter) {
+		List<E> result = new ArrayList<E>();
 		int index = -1;
 		for(E ele : list) {
 			boolean flag = listFilter.predicate(ele,++index,list);
@@ -21,4 +33,14 @@ public class CollectionUtil {
 		}
 		return result;
 	}
+
+	public static<E,K> K reduce(List<E> list, ListReducer<E,K> listReducer, K pv) {
+		int index = -1;
+		for(E ele : list) {
+			pv = listReducer.reducer(pv,ele,++index,list);
+		}
+		return pv;
+	}
+
+	
 }
--- CollectionUtilTest.java
+++ CollectionUtilTest.java
@@ -2,21 +2,24 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import java.util.*;
 
-interface ListMapper<E> {
-	E callback(E ele, int index, List<E> list);
-}
-
-interface ListFilter<E> {
-	boolean predicate(E ele, int index, List<E> list);
-}
-
-class SquareMapper implements ListMapper<Integer> {
+class SquareMapper implements ListMapper<Integer,Integer> {
 	public Integer callback (Integer ele, int index, List<Integer> list){
 		return ele.intValue()*ele.intValue();
 	}
 }
 
-class LowerCaseMapper implements ListMapper<String> {
+class SpellingMapper implements ListMapper<Integer,String> {
+	public String callback (Integer ele, int index, List<Integer> list){
+		switch(ele){
+			case 1: return "One";
+			case 2: return "Two";
+			case 3: return "Three";
+			default: return "";
+		}
+	}
+}
+
+class LowerCaseMapper implements ListMapper<String,String> {
 	public String callback (String ele, int index, List<String> list){
 		return ele.toLowerCase();
 	}
@@ -28,11 +31,16 @@ class EvenFilter implements ListFilter<Integer> {
 	}
 }
 
+class SumReducer implements ListReducer<Integer,Integer> {
+	public Integer reducer (Integer pv, Integer cv , int index, List<Integer> list) {
+		return pv.intValue()+cv.intValue();
+	}
+}
 
 public class CollectionUtilTest {
 	@Test
 	public void map_returns_an_Integer_List_after_Sqauring_on_the_given_list(){
-		ListMapper listMapper = new SquareMapper();
+		ListMapper<Integer,Integer> listMapper = new SquareMapper();
 		List<Integer> numbers = new ArrayList<Integer>();
 		List<Integer> expected = new ArrayList<Integer>();
 		
@@ -44,14 +52,14 @@ public class CollectionUtilTest {
 		expected.add(4);
 		expected.add(9);
 
-		List<Integer> result = CollectionUtil.<Integer>map(numbers,listMapper);
+		List<Integer> result = CollectionUtil.<Integer,Integer>map(numbers,listMapper);
 		assertEquals(expected.get(0),result.get(0));
 		assertEquals(expected.get(1),result.get(1));
 	}
 
 	@Test
 	public void map_returns_a_String_List_after_lowerCasing_on_the_given_list(){
-		ListMapper listMapper = new LowerCaseMapper();
+		ListMapper<String,String> listMapper = new LowerCaseMapper();
 		List<String> names = new ArrayList<String>();
 		List<String> expected = new ArrayList<String>();
 		
@@ -61,14 +69,33 @@ public class CollectionUtilTest {
 		expected.add("bob");
 		expected.add("alice");
 
-		List<String> result = CollectionUtil.<String>map(names,listMapper);
+		List<String> result = CollectionUtil.<String,String>map(names,listMapper);
+		assertEquals(expected.get(0),result.get(0));
+		assertEquals(expected.get(1),result.get(1));
+	}
+
+	@Test
+	public void map_returns_a_list_of_spellings_of_given_numbers(){
+		ListMapper<Integer,String> listMapper = new SpellingMapper();
+		List<Integer> numbers = new ArrayList<Integer>();
+		List<String> expected = new ArrayList<String>();
+		
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+
+		expected.add("One");
+		expected.add("Two");
+		expected.add("Three");
+
+		List<String> result = CollectionUtil.<Integer,String>map(numbers,listMapper);
 		assertEquals(expected.get(0),result.get(0));
 		assertEquals(expected.get(1),result.get(1));
 	}
 
 	@Test
 	public void filter_returns_an_Integer_List_after_filtering_evens_from_the_given_list(){
-		ListFilter listFilter = new EvenFilter();
+		ListFilter<Integer> listFilter = new EvenFilter();
 		List<Integer> names = new ArrayList<Integer>();
 		List<Integer> expected = new ArrayList<Integer>();
 		
@@ -83,5 +110,20 @@ public class CollectionUtilTest {
 		List<Integer> result = CollectionUtil.<Integer>filter(names,listFilter);
 		assertEquals(expected.get(0),result.get(0));
 		assertEquals(expected.get(1),result.get(1));
+	}
+	
+	@Test
+	public void reduce_returns_the_sum_of_all_numbers (){
+		ListReducer<Integer,Integer> listReducer = new SumReducer();
+		List<Integer> numbers = new ArrayList<Integer>();
+		Integer initial = new Integer(0);
+		Integer expected = new Integer(6);
+		
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+
+		Integer sum = CollectionUtil.<Integer,Integer>reduce(numbers,listReducer,initial);		
+		assertEquals(expected,sum);
 	}
 }
