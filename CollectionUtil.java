8d8cd614b031142ee9ba7be94723e5f1259d1dfc
Dolly <dollyg@INDOLLYG.corporate.thoughtworks.com>
2015-02-28 11:34:21 +0530

    filter implemented

--- CollectionUtil.java
+++ CollectionUtil.java
@@ -3,9 +3,21 @@ import java.util.*;
 public class CollectionUtil {
 	public static<E> List<E> map(List<E> list, ListMapper listMapper) {
 		List<E> result = new ArrayList();
+		int index = -1;
 		for(E ele : list) {
-			E i = (E)listMapper.mapCallback(ele,0,list);
+			E i = (E)listMapper.callback(ele,++index,list);
 			result.add(i);
+		}
+		return result;
+	}
+
+	public static<E> List<E> filter(List<E> list, ListFilter listFilter) {
+		List<E> result = new ArrayList();
+		int index = -1;
+		for(E ele : list) {
+			boolean flag = listFilter.predicate(ele,++index,list);
+			if(flag)
+				result.add(ele);
 		}
 		return result;
 	}
--- CollectionUtilTest.java
+++ CollectionUtilTest.java
@@ -3,18 +3,28 @@ import org.junit.Test;
 import java.util.*;
 
 interface ListMapper<E> {
-	E mapCallback(E ele, int index, List<E> list);
+	E callback(E ele, int index, List<E> list);
 }
 
-class IntegerMapper implements ListMapper<Integer> {
-	public Integer mapCallback (Integer ele, int index, List<Integer> list){
+interface ListFilter<E> {
+	boolean predicate(E ele, int index, List<E> list);
+}
+
+class SquareMapper implements ListMapper<Integer> {
+	public Integer callback (Integer ele, int index, List<Integer> list){
 		return ele.intValue()*ele.intValue();
 	}
 }
 
-class StringMapper implements ListMapper<String> {
-	public String mapCallback (String ele, int index, List<String> list){
+class LowerCaseMapper implements ListMapper<String> {
+	public String callback (String ele, int index, List<String> list){
 		return ele.toLowerCase();
+	}
+}
+
+class EvenFilter implements ListFilter<Integer> {
+	public boolean predicate (Integer ele, int index, List<Integer> list){
+		return (ele.intValue() % 2 == 0);
 	}
 }
 
@@ -22,7 +32,7 @@ class StringMapper implements ListMapper<String> {
 public class CollectionUtilTest {
 	@Test
 	public void map_returns_an_Integer_List_after_Sqauring_on_the_given_list(){
-		ListMapper listMapper = new IntegerMapper();
+		ListMapper listMapper = new SquareMapper();
 		List<Integer> numbers = new ArrayList<Integer>();
 		List<Integer> expected = new ArrayList<Integer>();
 		
@@ -41,7 +51,7 @@ public class CollectionUtilTest {
 
 	@Test
 	public void map_returns_a_String_List_after_lowerCasing_on_the_given_list(){
-		ListMapper listMapper = new StringMapper();
+		ListMapper listMapper = new LowerCaseMapper();
 		List<String> names = new ArrayList<String>();
 		List<String> expected = new ArrayList<String>();
 		
@@ -52,6 +62,25 @@ public class CollectionUtilTest {
 		expected.add("alice");
 
 		List<String> result = CollectionUtil.<String>map(names,listMapper);
+		assertEquals(expected.get(0),result.get(0));
+		assertEquals(expected.get(1),result.get(1));
+	}
+
+	@Test
+	public void filter_returns_an_Integer_List_after_filtering_evens_from_the_given_list(){
+		ListFilter listFilter = new EvenFilter();
+		List<Integer> names = new ArrayList<Integer>();
+		List<Integer> expected = new ArrayList<Integer>();
+		
+		names.add(1);
+		names.add(2);
+		names.add(4);
+		names.add(9);
+
+		expected.add(2);
+		expected.add(4);
+
+		List<Integer> result = CollectionUtil.<Integer>filter(names,listFilter);
 		assertEquals(expected.get(0),result.get(0));
 		assertEquals(expected.get(1),result.get(1));
 	}
